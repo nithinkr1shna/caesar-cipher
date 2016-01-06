65293c84754f54ae5f5d2fd56c25e029df53ce9e
Nithin <nithinkrishna222@gmail.com>
2016-01-06 16:04:31 +0530

    file handlers closed

--- fileop.py
+++ fileop.py
@@ -1,12 +1,14 @@
 class FileOperator(object):
 
 	def file_write(self,content):
-		file_w = open("out.txt",'a')
+		file_w = open('out.txt','a')
 		file_w.write(content)
+		file_w.close()
 
 	def file_read(self):
-		file_r= open("pg.txt",'r')
-		return file_r.read()
-
+		file_r= open('pg.txt','r')
+		file_content = file_r.read()
+		file_r.close()
+		return file_content
 
 
