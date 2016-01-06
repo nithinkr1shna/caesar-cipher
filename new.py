bdcb6c8817e5323248356197703a8182b58cd713
Nithin <nithinkrishna222@gmail.com>
2016-01-06 13:06:09 +0530

    issue #2 cleared

--- new.py
+++ new.py
@@ -1,20 +1,14 @@
 import fileop
-
+import process
 
 class Caeser(object):
 	def __init__(self,shift):
 		self.shift = shift
 
-
 	def encrypt(self):
 		data = fileop.FileOperator().file_read()
-		for var in data:
-			if var.isalpha():
-				to_write = ord(var) + self.shift
-				if(to_write > ord('z')):
-					to_write=to_write-26
-				to_file = chr(to_write)
-				fileop.FileOperator().file_write(to_file)
+		to_file = process.Proces().processEncrypt(data,self.shift)
+		fileop.FileOperator().file_write(to_file)
 
 
 new = Caeser(3)
--- process.py
+++ process.py
@@ -1,10 +1,11 @@
 class Proces:
 	def processEncrypt(self,data,shift):
-		to_file = ""
+		to_file = " "
+		NUMBER_26 = ord('z')
 		for var in data:
 			if var.isalpha():
 				to_write = ord(var) + shift
-				if(to_write > ord('z')):
+				if(to_write > NUMBER_26):
 					to_write=to_write-26
-				to_file =to_file+ chr(to_write)
+				to_file +=chr(to_write)
 		return to_file
